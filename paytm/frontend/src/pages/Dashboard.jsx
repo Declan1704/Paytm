a0c3825d9cac6373a7fcbee8429958117fb02e27
Declan1704 <22u1269@students.git.edu>
2025-02-05 19:51:39 +0530

    My paytm full working

--- paytm/frontend/src/pages/Dashboard.jsx
+++ paytm/frontend/src/pages/Dashboard.jsx
@@ -7,14 +7,20 @@ import axios from "axios";
 const Dashboard=()=>{
     const [Val,setVal]=useState(0);
     useEffect(()=>{
-        setVal(axios.get("http://localhost:3000/api/v1/account/balance"))
+        axios.get("http://localhost:3000/api/v1/account/balance",{
+            headers:{
+                authorization:localStorage.getItem("token")
+            }
+        }).then(function(res){
+            setVal(res.data.balance)
+        })
     },[])
     console.log(Val)
     return(
         <>
             <Appbar />
             <div className="m-8">
-                <Balance value="1000" />
+                <Balance value={Val} />
                 <Users />
             </div>
         </>
